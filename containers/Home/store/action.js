f58504ee2e1c01242d3fed78d60878f8f036e499
郭帅帅 <704555087@qq.com>
2019-09-20 17:55:14 +0800

    修改port

--- containers/Home/store/action.js
+++ containers/Home/store/action.js
@@ -8,7 +8,7 @@ const home_list_action = (data) => ({
 
 export const fetch_home_list = () => {
     return (dispatch) => {
-        return axios.get('http://127.0.0.1:8002/home_list')
+        return axios.get('http://127.0.0.1:8002/proxy/home_list')
             .then((res) => {
                 dispatch(home_list_action({
                     list: res.data
